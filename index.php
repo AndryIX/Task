d7e239279376ed61ab91c6b2869dad92eb13694c
MIKURI <and.iks9034@gmail.com>
2023-10-22 17:56:04 +0400

    UPD3

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $url = "http://my-json-server.typicode.com/AndryIX/Task/users";
 curl_setopt_array($ch, [
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_URL => $url,
+    CURLOPT_TIMEOUT => 90,
 ]);
 
 $response = curl_exec($ch);
@@ -14,6 +15,6 @@ curl_close($ch);
 
 $obj_response = json_decode($response);
 
-foreach($obj_response as $response){
-    echo $response -> username . '<br>';
+foreach($obj_response as $value){
+    echo $value -> username . '<br>';
 }
