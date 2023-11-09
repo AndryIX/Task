6e9edec2f4c2ea8647ba304ee410708d03347d33
MIKURI <and.iks9034@gmail.com>
2023-11-09 15:19:11 +0400

    db another edit

--- usersInfo.php
+++ usersInfo.php
@@ -3,11 +3,16 @@ require_once "UserApi.php";
 use Api\UserApi;
 
 $users = UserApi::getUsers();
-//$posts = UserApi::getPosts();
-//$tasks = UserApi::getTasks();
+$posts = UserApi::getPosts();
+$tasks = UserApi::getTasks();
 
-foreach($users as $value){
+foreach($users as $uservalue){
     echo '<div class="card">';
-    echo '<h3>Пользователь: '.$value -> username.'</h3>';
+    echo '<h3>Пользователь: '.$uservalue -> username.'</h3>';
+    echo '<h3>Посты: </h3>';
+    foreach($posts as $postvalue){
+        if($uservalue -> id == $postvalue -> userID) echo '<p>'.$postvalue -> .'</p>';
+            
+    }
     echo '</div>';
 }
